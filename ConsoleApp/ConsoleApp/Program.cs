d3dd02bccc1c631bcb326af4f0f5c3a75e697201
mcb <907425636@qq.com>
2023-06-21 19:41:18 +0800

    Update

--- ConsoleApp/ConsoleApp/Program.cs
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -5,18 +5,22 @@ using System.Collections.Generic;
 
 Console.WriteLine("Hello, World!");
 
-int[] arr = new int[] { 4, 2, 13, 5, 1, 0, 10, 8, 7, 7, 7 };
+//int[] arr = new int[] { 4, 2, 13, 5, 1, 0, 10, 8, 7, 7, 7 };
 
 //Utils.PrintOddTimesNumFromArrayThereIsOne(1, 2, 2, 2, 2, 1, 1, 3, 3, 4, 4, 1, 5, 1, 5);
 //Utils.PrintOddTimesNumFromArrayThereAreTwo(2, 3, 4, 5, 6, 2, 3, 5, 7, 7, 8, 8);
 
-SortingUtils.InsertSort(arr);
+//SortingUtils.InsertSort(arr);
 //for (int i = 0; i < arr.Length; i++) {
 //	Console.Write(arr[i] + ",");
 //	Console.WriteLine();
 //}
 //bool getIt = Utils.FindNumInSortedArrayWithBisection(arr, 9);
-int getIt = Utils.FindLeftMostIndexBiggerThanNumInSortedArrayWithBisection(arr, 6);
+//int getIt = Utils.FindLeftMostIndexBiggerThanNumInSortedArrayWithBisection(arr, 6);
+
+int[] arr = new int[] { 5,1,2,6,3,1,4,5,1,2,1 };
+//int smallSum = CommonUtils.SmallSum(arr);
+CommonUtils.PrintReversePair(arr);
 
 Console.WriteLine();
 
--- ConsoleApp/ConsoleApp/Utils/CommonUtils.cs
+++ ConsoleApp/ConsoleApp/Utils/CommonUtils.cs
@@ -6,15 +6,6 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp.Utils {
 	public static class CommonUtils {
-		private static int GetMax(int[] _arr, int _L, int _R) {
-			if (_L == _R) {
-				return _arr[_L];
-			}
-			int mid = _L + ((_R - _L) >> 1);	// 【重点】
-			int leftMax = GetMax(_arr, _L, mid);
-			int rightMax = GetMax(_arr, mid + 1, _R);
-			return Math.Max(leftMax, rightMax);
-		}
 		/// <summary>
 		/// 获取数组中最大的值
 		/// </summary>
@@ -25,6 +16,105 @@ namespace ConsoleApp.Utils {
 				return Int32.MinValue;
 			}
 			return GetMax(_arr, 0, _arr.Length - 1);
+		}
+
+		private static int GetMax(int[] _arr, int _L, int _R) {
+			if (_L == _R) {
+				return _arr[_L];
+			}
+			int mid = _L + ((_R - _L) >> 1);    // 【重点】
+			int leftMax = GetMax(_arr, _L, mid);
+			int rightMax = GetMax(_arr, mid + 1, _R);
+			return Math.Max(leftMax, rightMax);
+		}
+
+		/// <summary>
+		/// 小和算法
+		/// </summary>
+		/// <param name="_arr"></param>
+		/// <returns></returns>
+		public static int SmallSum(int[] _arr) {
+			if (_arr == null || _arr.Length == 0) {
+				return 0;
+			}
+			return SmallSum(_arr, 0, _arr.Length - 1);
+		}
+
+		private static int SmallSum(int[] _arr, int _L, int _R) {
+			if (_L == _R) {
+				return 0;
+			}
+			int mid = _L + ((_R - _L) >> 1);
+			return SmallSum(_arr, _L, mid) + SmallSum(_arr, mid + 1, _R) + SmallSumMerge(_arr, _L, mid, _R);
+		}
+
+		private static int SmallSumMerge(int[] _arr, int _L, int _M, int _R) {
+			int[] help = new int[_R - _L + 1];
+			int i = 0;
+			int p1 = _L;
+			int p2 = _M + 1;
+			int sum = 0;
+			while (p1 <= _M && p2 <= _R) {
+				// 【注意】遇到左右侧相等的数时，右侧的数优先进入数组
+				// 【注意】无法保留相同值的前后顺序
+				bool condition = _arr[p1] < _arr[p2];
+				sum += condition ? _arr[p1] * (_R - p2 + 1) : 0;
+				help[i++] = condition ? _arr[p1++] : _arr[p2++];
+			}
+			while (p1 <= _M) {
+				help[i++] = _arr[p1++];
+			}
+			while (p2 <= _R) {
+				help[i++] = _arr[p2++];
+			}
+			// 结果返回
+			for (i = 0; i < help.Length; i++) {
+				_arr[_L + i] = help[i];
+			}
+			return sum;
+		}
+
+		/// <summary>
+		/// 打印逆序对
+		/// </summary>
+		/// <param name="_arr"></param>
+		public static void PrintReversePair(int[] _arr) {
+			if (_arr == null || _arr.Length == 0) return;
+			PrintReversePair(_arr, 0, _arr.Length - 1);
+		}
+
+		private static void PrintReversePair(int[] _arr, int _L, int _R) {
+			if (_L == _R) return;
+			int mid = _L + ((_R - _L) >> 1);
+			PrintReversePair(_arr, _L, mid);
+			PrintReversePair(_arr, mid + 1, _R);
+			PrintReversePairMerge(_arr, _L, mid, _R);
+		}
+
+		private static void PrintReversePairMerge(int[] _arr, int _L, int _M, int _R) {
+			int[] help = new int[_R - _L + 1];
+			int i = 0;
+			int p1 = _L;
+			int p2 = _M + 1;
+			while (p1 <= _M && p2 <= _R) {
+				// 【注意】需保留相同值的前后顺序
+				bool condition = _arr[p1] <= _arr[p2];
+				if (condition == false) {
+					for (int j = p1; j <= _M; j++) {
+						Console.WriteLine("({0}, {1})", _arr[j], _arr[p2]);
+					}
+				}
+				help[i++] = condition ? _arr[p1++] : _arr[p2++];
+			}
+			while (p1 <= _M) {
+				help[i++] = _arr[p1++];
+			}
+			while (p2 <= _R) {
+				help[i++] = _arr[p2++];
+			}
+			for (i = 0; i < help.Length; i++) {
+				_arr[_L + i] = help[i];
+			}
 		}
 	}
 }
